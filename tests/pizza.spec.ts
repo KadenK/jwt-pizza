ecae8ea00b82f27aeef8dd47cea47eb3f5781aa5
Kaden Keep <kaden.keep@gmail.com>
2025-10-07 20:53:47 -0600

    initial playwright setup

---
+++ tests/pizza.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "playwright-test-coverage";
+import { Page } from "playwright/test";
+
+test("home page", async ({ page }) => {
+  await page.goto("/");
+
+  expect(await page.title()).toBe("JWT Pizza");
+});
